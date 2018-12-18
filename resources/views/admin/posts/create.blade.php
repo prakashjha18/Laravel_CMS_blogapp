acfddc976152b916c0d250014657498b253c1f0c
Unknown <prkjha25@gmail.com>
2018-12-18 14:18:00 +0530

    creating the post view

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+
+@section('content')
+    <h2 class="text-center">
+    	Create new Post
+    </h2>	
+
+@stop
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/post/create', [
+   'uses' => 'PostsController@create',
+   'as'   => 'post.create'
+]);
