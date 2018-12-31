49de0f0accee1388031bdc71f28da2fecbe2c21f
Unknown <prkjha25@gmail.com>
2018-12-31 23:55:12 +0530

    displaying categories in form

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Category;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
@@ -23,7 +23,7 @@ class PostsController extends Controller
      */
     public function create()
     {
-        return view('admin.posts.create');
+        return view('admin.posts.create')->with('categories',Category::all());
     }
 
     /**
@@ -39,7 +39,8 @@ class PostsController extends Controller
         $this->validate($request, [
             'title' => 'required|',
             'featured' => 'required|image',
-            'content'  => 'required'
+            'content'  => 'required',
+            'category_id' => 'required'
         ]);
 
         dd($request->all());
