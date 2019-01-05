73fcc3f3aa3ed75d4fdebef5d7556bb6c370b805
Unknown <prkjha25@gmail.com>
2019-01-05 19:40:31 +0530

    dashboard and closing

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Post;
+use App\User;
+use App\Category;
 
 use Illuminate\Http\Request;
 
@@ -20,6 +23,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('admin.dashboard')
+                    ->with('posts_count',Post::all()->count())
+                    ->with('trashed_count',Post::onlyTrashed()->get()->count())
+                    ->with('users_count',User::all()->count())
+                    ->with('categories_count',Category::all()->count());
     }
 }
