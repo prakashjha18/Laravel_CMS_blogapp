d159235dd2e525d8f6dc15b6464e1b2e65f44a36
Unknown <prkjha25@gmail.com>
2019-01-05 13:51:40 +0530

    included search

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -446,20 +446,20 @@
 <!-- Overlay Search -->
 
 <div class="overlay_search">
-    <div class="container">
-        <div class="row">
-            <div class="form_search-wrap">
-                <form>
-                    <input class="overlay_search-input" placeholder="Type and hit Enter..." type="text">
-                    <a href="#" class="overlay_search-close">
-                        <span></span>
-                        <span></span>
-                    </a>
-                </form>
+        <div class="container">
+            <div class="row">
+                <div class="form_search-wrap">
+                    <form method="GET" action="/results">
+                        <input class="overlay_search-input" name="query" placeholder="Type and hit Enter..." type="text">
+                        <a href="#" class="overlay_search-close">
+                            <span></span>
+                            <span></span>
+                        </a>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
 <!-- End Overlay Search -->
 
